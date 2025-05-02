65e34258bb0fad5069df32aaa93fe381cdcfc829
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-05-02 21:07:06 +0900

    Adding monthly average rentals and day of the week average rentals

--- app.py
+++ app.py
@@ -31,3 +31,17 @@ hourly_avg = filtered_df.groupby('Hour')['Rented Bike Count'].mean().reset_index
 fig1, ax1 = plt.subplots()
 sns.lineplot(data=hourly_avg, x='Hour', y='Rented Bike Count', marker='o', ax=ax1)
 st.pyplot(fig1)
+
+st.subheader("📅 Average Rentals by Month")
+monthly_avg = filtered_df.groupby('Month')['Rented Bike Count'].mean().reset_index()
+fig2, ax2 = plt.subplots()
+sns.barplot(data=monthly_avg, x='Month', y='Rented Bike Count', ax=ax2)
+ax2.set_xticks(range(1,13))
+st.pyplot(fig2)
+
+st.subheader("🗓️ Average Rentals by Day of Week")
+day_order = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+dow_avg = filtered_df.groupby('Day of Week')['Rented Bike Count'].mean().reindex(day_order).reset_index()
+fig3, ax3 = plt.subplots()
+sns.barplot(data=dow_avg, x='Day of Week', y='Rented Bike Count', ax=ax3)
+st.pyplot(fig3)
